f47950bc49a2d4992b1bd4098670652547c536d5
Anwesh43 <anweshthecool0@gmail.com>
2021-04-17 20:57:34 +0530

    adding VArcStroke class

--- index.ts
+++ index.ts
@@ -197,3 +197,26 @@ class VASNode {
         return this 
     }
 }
+
+class VArcStroke {
+
+    curr : VASNode = new VASNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
