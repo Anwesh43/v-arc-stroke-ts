89802a7667c69fedc23abd6c65e250df29335c0a
Anwesh43 <anweshthecool0@gmail.com>
2021-04-17 20:55:35 +0530

    adding VASNode class

--- index.ts
+++ index.ts
@@ -155,3 +155,45 @@ class Animator {
         }
     }
 }
+
+class VASNode {
+
+    next : VASNode 
+    prev : VASNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new VASNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawVASNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : VASNode {
+        var curr : VASNode = this.prev
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
